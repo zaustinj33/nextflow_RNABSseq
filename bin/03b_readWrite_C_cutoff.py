99d4cf466691ce8ed954be6fe276d8d3dd34f873
Zac Johnson <43764080+zaustinj33@users.noreply.github.com>
2022-03-31 08:24:49 -0600

    test

--- bin/03b_readWrite_C_cutoff.py
+++ bin/03b_readWrite_C_cutoff.py
@@ -54,6 +54,6 @@ def count_C(samfile):
 
 
 #%%
-bam_file = path + "/working_data/" + sample_name + "/" + sample_name + "_genomeMap_sorted.bam"
+bam_file = sample_name + "_genomeMap_sorted.bam"
 print(bam_file)
 count_C(bam_file)
